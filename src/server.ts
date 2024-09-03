486cb4bceda6dc673fb42cd744422628d4478458
salmanbytewrap <80086418+salmanbytewrap@users.noreply.github.com>
2024-09-03 11:03:06 +0530

    Add logger and health check

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,20 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from 'express';
+import * as tracer from "tracer";
+import statusMonitor from 'express-status-monitor';
 import router from './router';
 import config from './config';
 import connectDB from './helpers/db';
 
 // Load environment variables from .env file
 const app = express();
-const port = config.PORT
+const port = config.PORT;
+const logger = tracer.colorConsole();
+
+
+app.use(statusMonitor()); // Monitor Server Status
+
 // Use built-in middleware for parsing JSON bodies
 app.use(express.json());
 
@@ -19,11 +26,26 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send(`Server is running on port ${port}`);
 })
-
+if (config.NODE_ENV === "development") {
+    app.use(express.text());
+    app.use((req, res, next) => {
+      logger.trace(`${req.method} ${req.url}`);
+      if (
+        (/json/i.test(req.headers?.["content-type"] ?? '') &&
+          Object.keys(req.body).length > 0) ||
+        req.body.length > 0
+      ) {
+        logger.debug(req.body);
+      }
+      next();
+    });
+  }
+  
 app.use(router) // Bind Main Router File with App
 
+
 // Connecting DB and Starting Server
-connectDB().then(()=>{
+connectDB().then(() => {
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     })
